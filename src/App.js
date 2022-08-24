be4f96aa7097bceb74cc50c4526da5b809af588f
scarfacedotcom <peteradaaku@gmail.com>
2022-08-24 23:28:09 +0100

    few changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { BrowserRouter as Router, Routes, Route, Redirect, Navigate } from 'react-router-dom'
+
+// styles
 import './App.css'
+
+// Page Components
+import Create from './pages/create/Create'
+import Dashboard from './pages/dashboard/Dashboard'
+import Login from './pages/login/Login'
+import Project from './pages/project/Project'
+import Signup from './pages/signup/Signup'
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route path='/' element= { <Dashboard /> } />
+            {/* <Route path='/' element= { <Dashboard /> } />
+            <Route path='/' element= { <Dashboard /> } />
+            <Route path='/' element= { <Dashboard /> } /> */}
+          </Routes>
+        </div>
+      </Router>
 
     </div>
   );
