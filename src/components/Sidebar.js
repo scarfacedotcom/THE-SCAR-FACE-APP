3f10659a16c3135d999f1f78349497a522eb65ab
scarfacedotcom <peteradaaku@gmail.com>
2022-08-25 21:02:09 +0100

    side bar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,36 @@
+import { NavLink } from 'react-router-dom'
+
+// Styles
+import './Sidebar.css'
+import DashboardIcon from '../assets/dashboard_icon.svg'
+import AddIcon from '../assets/add_icon.svg'
+
+
+export default function Sidebar() {
+  return (
+    <div className='sidebar'>
+      <div className="sidebar-content">
+        <div className="user">
+          <p>Hey user</p>
+        </div>
+        <nav className="links">
+          <ul>
+            <li>
+              <NavLink to='/'>
+                <img src={DashboardIcon} alt="dashboard icon" />
+                <span>Dash Board</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/create'>
+                <img src={AddIcon} alt="add project icon" />
+                <span>New Project</span>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+      </div> 
+    </div>
+  )
+}
