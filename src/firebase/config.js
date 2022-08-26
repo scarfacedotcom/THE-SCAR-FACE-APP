3298e4bf03d993df69c35b5ad774c441051a9854
scarfacedotcom <peteradaaku@gmail.com>
2022-08-26 06:30:42 +0100

    firebas storage

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import 'firebase/firestore'
 import 'firebase/auth'
+import 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: "AIzaSyCrRlRhTFFCns3FnSq4OR_Zm9jmvXV5YlA",
@@ -17,8 +18,9 @@ firebase.initializeApp(firebaseConfig)
 // init srevices
 const projectFirestore = firebase.firestore()
 const projectAuth = firebase.auth()
+const projectStorage = firebase.storage()
 
 // timestamp
 const timestamp = firebase.firestore.Timestamp
 
-export { projectFirestore, projectAuth, timestamp }
+export { projectFirestore, projectAuth, timestamp, projectStorage }
