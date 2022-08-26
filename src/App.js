3e33c833452b4acda2a220cec3d6057d332df46d
scarfacedotcom <peteradaaku@gmail.com>
2022-08-26 04:27:05 +0100

    signup page

--- src/App.js
+++ src/App.js
@@ -1,37 +1,3 @@
-// import { BrowserRouter as Router, Routes, Route, Redirect, Navigate } from 'react-router-dom'
-
-// // styles
-// import './App.css'
-
-// // Page Components
-// import Create from './pages/create/Create'
-// import Dashboard from './pages/dashboard/Dashboard'
-// import Login from './pages/login/Login'
-// import Project from './pages/project/Project'
-// import Signup from './pages/signup/Signup'
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Router>
-//         <div className="container">
-//           <Routes>
-//             <Route path='/' element= { <Dashboard /> } />
-//             <Route path='/create' element= { <Create /> } />
-//             <Route path='/projects/:id' element= { <Project /> } />
-//             <Route path='/login' element= { <Login /> } />
-//             <Route path='/signup' element= { <Signup /> } />
-//           </Routes>
-//         </div>
-//       </Router>
-
-//     </div>
-//   );
-// }
-
-// export default App
-
-
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 // styles
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,7 +1,13 @@
+import { useState } from 'react'
 import './Signup.css'
 
 
 export default function Signup() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [displayName, setDisplayName]= useState('')
+  const [thumbnail, setThumbnail] = useState(null)
+
   return (
     <div>
       Sign up
