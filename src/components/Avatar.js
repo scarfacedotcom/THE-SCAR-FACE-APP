863fca3288d227961cd513770366fe99dd12251b
scarfacedotcom <peteradaaku@gmail.com>
2022-08-29 04:17:50 +0100

    avatar component

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -6,7 +6,7 @@ import './Avatar.css'
 export default function Avatar({ src }) {
   return (
     <div className='avatar'>
-      {/* <img src={src} alt="avatar image" /> */}
+      <img src={src} alt="avatar image" />
       
     </div>
   )
