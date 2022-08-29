3bf1ea20c270c1e18247cfdb6afbf6e965cf7868
scarfacedotcom <peteradaaku@gmail.com>
2022-08-29 22:20:15 +0100

    few changes

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,59 +1,123 @@
+// import { useState } from 'react'
+
+// //styles
+// import './Create.css'
+
+
+// export default function Create() {
+//   const [name, setName] = useState('')
+//   const [details, setDetails] = useState('')
+//   const [dueDate, setDueDate] = useState('')
+//   const [category, setCategory] = useState('')
+//   const [assignedUsers, setAssignedUsers] = useState([])
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault()
+
+//     console.log(name,details,dueDate)
+//   }
+
+//   return (
+//     <div className='create-form'>
+//       <h2 className="page-title">Create a New Project</h2>
+//       <form onSubmit={handleSubmit}>
+//         <label>
+//           <span>Project Name:</span>
+//           <input 
+//             required
+//             type="text"
+//             value={name}
+//             onChange={(e) => setName(e.target.value)} 
+//           />
+//         </label>
+//         <label>
+//           <span>Project Details:</span>
+//           <textarea 
+//             required
+//             value={details}
+//             onChange={(e) => setDetails(e.target.value)} 
+//           /><textarea/>
+//         </label>
+//         <label>
+//           <span>Due Date:</span>
+//           <input 
+//             required
+//             type="date"
+//             value={dueDate}
+//             onChange={(e) => setDueDate(e.target.value)} 
+//           />
+//         </label>
+//         <label>
+//           <span>Project Category</span>
+//         </label>
+//         <label>
+//           <span>Assign to:</span>
+//         </label>
+//         <button className="btn">Add Project</button>
+//       </form>
+//     </div>
+//   )
+// }
+
 import { useState } from 'react'
 
-//styles
+// styles
 import './Create.css'
 
-
 export default function Create() {
+  // form field values
   const [name, setName] = useState('')
   const [details, setDetails] = useState('')
   const [dueDate, setDueDate] = useState('')
   const [category, setCategory] = useState('')
   const [assignedUsers, setAssignedUsers] = useState([])
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
 
-    console.log(name,details,dueDate)
+    console.log(name, details, dueDate)
   }
 
   return (
-    <div className='create-form'>
-      <h2 className="page-title">Create a New Project</h2>
+    <div className="create-form">
+      <h2 className="page-title">Create a new Project</h2>
       <form onSubmit={handleSubmit}>
         <label>
-          <span>Project Name:</span>
-          <input 
-            required
-            type="text"
+          <span>Project name:</span>
+          <input
+            required 
+            type="text" 
+            onChange={(e) => setName(e.target.value)}
             value={name}
-            onChange={(e) => setName(e.target.value)} 
           />
         </label>
         <label>
           <span>Project Details:</span>
           <textarea 
             required
-            type="text"
-            value={details}
-            onChange={(e) => setDetails(e.target.value)} 
-          /><textarea/>
+            onChange={(e) => setDetails(e.target.value)}
+            value={details} 
+          ></textarea>
         </label>
         <label>
-          <span>Due Date:</span>
-          <input 
-            required
-            type="date"
-            value={dueDate}
+          <span>Set due date:</span>
+          <input
+            required 
+            type="date" 
             onChange={(e) => setDueDate(e.target.value)} 
+            value={dueDate}
           />
         </label>
         <label>
-          <span>Project Category</span>
+          <span>Project category:</span>
+          {/* select here later */}
         </label>
         <label>
           <span>Assign to:</span>
+          {/* select here later */}
         </label>
+
+        <button className="btn">Add Project</button>
       </form>
     </div>
   )
