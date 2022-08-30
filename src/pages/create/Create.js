6fba0b71b2c2531422c6c0488679365b89edc494
scarfacedotcom <peteradaaku@gmail.com>
2022-08-30 04:39:55 +0100

    project category options

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,68 +1,15 @@
-// import { useState } from 'react'
-
-// //styles
-// import './Create.css'
-
-
-// export default function Create() {
-//   const [name, setName] = useState('')
-//   const [details, setDetails] = useState('')
-//   const [dueDate, setDueDate] = useState('')
-//   const [category, setCategory] = useState('')
-//   const [assignedUsers, setAssignedUsers] = useState([])
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault()
-
-//     console.log(name,details,dueDate)
-//   }
-
-//   return (
-//     <div className='create-form'>
-//       <h2 className="page-title">Create a New Project</h2>
-//       <form onSubmit={handleSubmit}>
-//         <label>
-//           <span>Project Name:</span>
-//           <input 
-//             required
-//             type="text"
-//             value={name}
-//             onChange={(e) => setName(e.target.value)} 
-//           />
-//         </label>
-//         <label>
-//           <span>Project Details:</span>
-//           <textarea 
-//             required
-//             value={details}
-//             onChange={(e) => setDetails(e.target.value)} 
-//           /><textarea/>
-//         </label>
-//         <label>
-//           <span>Due Date:</span>
-//           <input 
-//             required
-//             type="date"
-//             value={dueDate}
-//             onChange={(e) => setDueDate(e.target.value)} 
-//           />
-//         </label>
-//         <label>
-//           <span>Project Category</span>
-//         </label>
-//         <label>
-//           <span>Assign to:</span>
-//         </label>
-//         <button className="btn">Add Project</button>
-//       </form>
-//     </div>
-//   )
-// }
-
 import { useState } from 'react'
+import Select from 'react-select'
 
 // styles
 import './Create.css'
+
+const categories = [
+  { value: 'development', label: 'Development' },
+  { value: 'design', label: 'Design' },
+  { value: 'sales', label: 'Sales' },
+  { value: 'marketing', label: 'Marketing' },
+]
 
 export default function Create() {
   // form field values
@@ -110,11 +57,16 @@ export default function Create() {
         </label>
         <label>
           <span>Project category:</span>
-          {/* select here later */}
+          <Select
+            onChange={(option) => setCategory(option)}
+            options={categories}
+          />
         </label>
         <label>
           <span>Assign to:</span>
-          {/* select here later */}
+          <Select
+            options={}
+          />
         </label>
 
         <button className="btn">Add Project</button>
