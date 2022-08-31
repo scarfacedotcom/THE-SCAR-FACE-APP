3b34bc2c1f379014ada10889ae2fdbc621d59041
scarfacedotcom <peteradaaku@gmail.com>
2022-08-31 06:13:46 +0100

    outputtin the project list

---
+++ src/components/ProjectList.js
@@ -0,0 +1,15 @@
+//styles
+import './ProjectList.css'
+
+
+export default function ProjectList({projects}) {
+
+  return (
+    <div>
+      {projects.length === 0 && <p>No Projects</p> }
+      {projects.map(project => {
+        
+      })} 
+    </div>
+  )
+}
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,12 +1,18 @@
-// styles
+import ProjectList from '../../components/ProjectList'
+import { useCollection } from '../../hooks/useCollection'
 
+// styles
 import './Dashboard.css'
 
 
 export default function Dashboard() {
+  const { documents, error } = useCollection('projects')
+
   return (
     <div>
-      Dashboard
+      <h2 className="page-title">Dashboard</h2>
+      {error && <p className="error">{error}</p> }
+      {documents && <ProjectList projects={documents}/> }
     </div>
   )
 }
