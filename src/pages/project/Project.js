6424e082b4998df6d7eb555ef8880db52bf390d2
scarfacedotcom <peteradaaku@gmail.com>
2022-09-01 05:53:12 +0100

    fetching the project

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,9 +1,23 @@
+import { useParams } from 'react-router-dom'
+import{ useDocument } from '../../hooks/useDocument'
+
+// styles
 import './Project.css'
 
 export default function Project() {
+  const { id } = useParams()
+
+  if(error) {
+    return <div className="error">{error}</div>
+  }
+
+  if (!document) {
+    return <div className="loading">loading...</div>
+  }
+
   return (
-    <div>
-      Project Details
+    <div className='project-details'>
+      <h1>{document.name}</h1>
     </div>
   )
 }
