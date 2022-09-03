a0af867305a248ebab4b8bfa199b82ba62241985
scarfacedotcom <peteradaaku@gmail.com>
2022-09-03 19:59:59 +0100

    few changes

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -1,12 +1,60 @@
+// // import { useState } from "react"
+// // import { useAuthContext } from "../../hooks/useAuthContext"
+// // import { timestamp } from "../../firebase/config"
+// // import { useFirestore } from "../../hooks/useFirestore"
+
+// // export default function ProjectComments({project}) {
+// //   const [newComment, setNewComment] = useState('')
+// //   const {user} = useAuthContext()
+// //   const { updateDocument, response } = useFirestore('projects')
+
+// //   const handleSubmit = async (e) => {
+// //     e.preventDefault();
+
+// //     const commentToAdd = {
+// //       displayName: user.displayName,
+// //       photoURL: user.photoURL,
+// //       content: newComment,
+// //       createdAt: timestamp.fromDate(new Date()),
+// //       id: Math.random()
+// //     }
+// //     await updateDocument(project.id, {
+// //       comments: [...project.comments, commentToAdd]
+// //     })
+// //     if(!response.error) {
+// //       setNewComment('')
+// //       }
+// //   }
+
+// //   return (
+// //     <div className="project-comments">
+// //       <h4>Project Comments</h4>
+// //       <form className="add-comment" onSubmit={handleSubmit}>
+// //         <label>
+// //           <span>Add New Comment:</span>
+// //           <textarea
+// //             required
+// //             onChange={(e) => setNewComment(e.target.value) }
+// //             value={newComment}
+// //           ></textarea>
+// //         </label>
+// //         <button className="btn">Add Comment</button>
+// //       </form>
+      
+// //     </div>
+// //   )
+// // }
+
+
 // import { useState } from "react"
-// import { useAuthContext } from "../../hooks/useAuthContext"
 // import { timestamp } from "../../firebase/config"
+// import { useAuthContext } from "../../hooks/useAuthContext"
 // import { useFirestore } from "../../hooks/useFirestore"
 
-// export default function ProjectComments({project}) {
-//   const [newComment, setNewComment] = useState('')
-//   const {user} = useAuthContext()
+// export default function ProjectComments({ project }) {
+//   const { user } = useAuthContext()
 //   const { updateDocument, response } = useFirestore('projects')
+//   const [newComment, setNewComment] = useState('')
 
 //   const handleSubmit = async (e) => {
 //     e.preventDefault();
@@ -18,33 +66,32 @@
 //       createdAt: timestamp.fromDate(new Date()),
 //       id: Math.random()
 //     }
+    
 //     await updateDocument(project.id, {
-//       comments: [...project.comments, commentToAdd]
+//       comments: [...project.comments, commentToAdd],
 //     })
-//     if(!response.error) {
+//     if (!response.error) {
 //       setNewComment('')
-//       }
+//     }
 //   }
 
 //   return (
 //     <div className="project-comments">
 //       <h4>Project Comments</h4>
+
 //       <form className="add-comment" onSubmit={handleSubmit}>
 //         <label>
-//           <span>Add New Comment:</span>
-//           <textarea
-//             required
-//             onChange={(e) => setNewComment(e.target.value) }
+//           <span>Add new comment:</span>
+//           <textarea 
+//             onChange={(e) => setNewComment(e.target.value)}
 //             value={newComment}
 //           ></textarea>
 //         </label>
 //         <button className="btn">Add Comment</button>
 //       </form>
-      
 //     </div>
 //   )
 // }
-
 
 import { useState } from "react"
 import { timestamp } from "../../firebase/config"
