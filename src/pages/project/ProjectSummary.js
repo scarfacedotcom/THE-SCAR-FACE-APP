d096e7a988c667d836f0ba73863bf7187cde2ab3
scarfacedotcom <peteradaaku@gmail.com>
2022-09-05 04:48:03 +0100

    few changes

--- src/pages/project/ProjectSummary.js
+++ src/pages/project/ProjectSummary.js
@@ -6,7 +6,7 @@ export default function ProjectSummary({ project }) {
   return (
     <>
       <div className="project-summary">
-        <h1 className="page-title">{project.name}</h1>
+        <h2 className="page-title">{project.name}</h2>
         <p className="due-date">
           Project due by {project.dueDate.toDate().toDateString()}
         </p>
@@ -14,7 +14,7 @@ export default function ProjectSummary({ project }) {
           Project  Details:
           {project.details}
         </p>
-        <h4>Project assigned to:</h4>
+        <h4>Project is assigned to:</h4>
           <div className="assigned-users">
             {project.assignedUsersList.map(user => (
               <div key={user.id}>
